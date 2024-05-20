639d929b3f84a8aeac5a415cc7130342c77753a6
Aditya <shrimaliaditya013@gmail.com>
2024-05-20 13:33:12 +0530

    added project

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,7 @@
 import Planner from "../assets/projects/travelPlanner.png";
 import Ecomzy from "../assets/projects/ecomzy.png";
 import RandomGif from "../assets/projects/randomgif.png";
+import TASK_M from "../assets/projects/task_m.png";
 
 export const data = [
   {
@@ -12,13 +13,20 @@ export const data = [
   },
   {
     id: 2,
+    name: "Task_Manager",
+    image: TASK_M,
+    github: "https://github.com/aditya-shrimali/task_m",
+    live: "https://task-m-frontend.vercel.app/",
+  },
+  {
+    id: 3,
     name: "Items Sale",
     image: Ecomzy,
     github: "https://github.com/aditya-shrimali/Items-Sale",
     // live: "https://shopping-cart-webapp.vercel.app/",
   },
   {
-    id: 3,
+    id: 4,
     name: "CashSync App",
     image: RandomGif,
     github: "https://github.com/aditya-shrimali/CashSync",
